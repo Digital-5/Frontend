e230cedd8dd395184fdbe46e8c6b95d7c8fa5be7
Liridon <liridon.bytyqi@itSchuleStuttgart1.onmicrosoft.com>
2025-11-20 14:29:41 +0100

    enhanced schemas

---
+++ theme/fonts.ts
@@ -0,0 +1,17 @@
+// Roboto Font Family Configuration
+export const Fonts = {
+  thin: 'Roboto_100Thin',
+  thinItalic: 'Roboto_100Thin_Italic',
+  light: 'Roboto_300Light',
+  lightItalic: 'Roboto_300Light_Italic',
+  regular: 'Roboto_400Regular',
+  italic: 'Roboto_400Regular_Italic',
+  medium: 'Roboto_500Medium',
+  mediumItalic: 'Roboto_500Medium_Italic',
+  bold: 'Roboto_700Bold',
+  boldItalic: 'Roboto_700Bold_Italic',
+  black: 'Roboto_900Black',
+  blackItalic: 'Roboto_900Black_Italic',
+};
+
+export default Fonts;
